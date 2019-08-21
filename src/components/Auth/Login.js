01852e3c9c2b2d686a9f8a2f7de7bedbddb9b0a4
yuki <yuki.hasegawa.s1@gmail.com>
2019-08-21 12:08:06 +0900

    module-2

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import firebase from "../../firebase";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+class Login extends React.Component {
+  
+    render(){
+        return (
+        <p>Login</p>
+        );
+    }
+}
+
+export default Login;
